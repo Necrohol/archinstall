649ad2cc144904f13d368611e10d7580d6dd6ba9
Michael L. <mike@michaellawrenceit.com>
2026-04-01 03:42:50 -0500

    Update __init__.py

--- archinstall/lib/disk/arch_handlers/__init__.py
+++ archinstall/lib/disk/arch_handlers/__init__.py
@@ -1,1 +1,11 @@
+from .x86_64 import X86_64Handler
+from .arm64 import Arm64Handler
+from .riscv64 import Riscv64Handler
+
+ARCH_MAP = {
+    'x86_64': X86_64Handler,
+    'aarch64': Arm64Handler,
+    'arm64': Arm64Handler,
+    'riscv64': Riscv64Handler
+}
 
