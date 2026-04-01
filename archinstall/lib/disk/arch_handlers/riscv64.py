2aebf4128218ae22e6cc46e0c2f286095b2ea133
Michael L. <mike@michaellawrenceit.com>
2026-04-01 03:31:54 -0500

    Update riscv64.py

--- archinstall/lib/disk/arch_handlers/riscv64.py
+++ archinstall/lib/disk/arch_handlers/riscv64.py
@@ -1,1 +1,11 @@
 
+class Riscv64DeviceHandler(BaseDeviceHandler):
+    def get_partition_table_type(self) -> PartitionTable:
+        return PartitionTable.GPT  # RISC-V almost always wants GPT/UEFI
+
+    def get_root_guid(self) -> str:
+        return "5BE22121-1D04-4143-9E04-511B6195E68C"
+
+    def get_boot_args(self) -> str:
+        # Specific to Milk-V / Banana Pi F3 serial consoles
+        return "console=ttyS0,115200 rootwait"
